b82036168362f24dd3fc8ad3946928508f932a81
Dmytro Korovin <d.korovin@rocketech.it>
2024-12-29 23:50:15 +0200

    fix device orientation

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,15 @@
 import 'package:beomy_login/app.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+  ]);
   await Firebase.initializeApp();
+
   runApp(BeomyApp());
 }
